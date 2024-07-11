d7ab2ca3c2fa4af8bb047f0b5e1bf4108e930a7b
AbdulrahmanMossad <abdulrahmanmosad670@gmail.com>
2024-07-11 14:50:18 +0300

    Domain added

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,5 +1,5 @@
 export const ARTICLE_PER_PAGE = 6
-const PRODUCTION_DOMAIN = "http://localhost:3000"
+const PRODUCTION_DOMAIN = "https://cloud-hosting-next-js.vercel.app"
 const DEVELOPMENT_DOMAIN = "http://localhost:3000"
 
 export const DOMAIN =
